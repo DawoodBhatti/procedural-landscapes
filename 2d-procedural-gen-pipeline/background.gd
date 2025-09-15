31b4cd128628c5c66c5734f50abd82aca9dcf61a
Ali <d.a.bhatti@hotmail.com>
2025-09-15 22:25:33 +0100

    background canvas added

---
+++ 2d-procedural-gen-pipeline/background.gd
@@ -0,0 +1,17 @@
+extends ColorRect
+
+@export var scale_factor: float = 2.0
+var shape_center: Vector2
+
+
+func _ready() -> void:
+	size = get_viewport().size * scale_factor
+	
+	# position sets the top left corner point of the ColorRect node
+	#so here we center the background at global position (0, 0)
+	position = -size/2  
+	
+	#and we set the shape center which can be accessed by other code
+	shape_center = size/2
+	
+	set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
